f6a5bb41be28abf5cfeb58c85ad1b029cb519069
Luis <luiizmc@gmail.com>
2020-02-19 11:15:13 -0600

    boton regreso en webview Places

--- ios/build/rn_oreo/Build/Intermediates.noindex/Pods.build/Debug-iphonesimulator/Pods-OneSignalNotificationServiceExtension.build/Objects-normal/x86_64/Pods-OneSignalNotificationServiceExtension-dummy.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/hugo/Downloads/GIT/yc_meincluyo/ios/Pods/Target\ Support\ Files/Pods-OneSignalNotificationServiceExtension/Pods-OneSignalNotificationServiceExtension-dummy.m
--- src/screens/places/places.js
+++ src/screens/places/places.js
@@ -1,12 +1,12 @@
 import React, {Component} from 'react';
 //import {WebView} from 'react-native';
 import { WebView } from 'react-native-webview';
-
+const WEBVIEW_REF = "WEBVIEW_REF";
 
 // import {connect} from 'react-redux';
 // import {DrawerActions} from 'react-navigation-drawer';
 
-import {ScrollView, View, Dimensions, Text} from 'react-native';
+import {ScrollView, View, Dimensions, Text, StyleSheet,TouchableOpacity} from 'react-native';
 
 import {ThemedView, Header} from 'src/components';
 import {TextHeader, IconHeader, Logo, CartIcon} from 'src/containers/HeaderComponent';
@@ -19,17 +19,59 @@ import {
 
 
 class Places extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { canGoBack: false };
+  }
+
     render() {
       return (
-        <WebView source={{ uri: 'https://me-incluyo.org/places/' }} 
-         style={{marginTop: 20}}
-         />
-        
-         
-    
+        <View style={styles.container}>
+        <View style={styles.topbar}>
+          <TouchableOpacity
+            disabled={!this.state.canGoBack}
+            onPress={this.onBack.bind(this)}
+            >
+            <Text style={this.state.canGoBack ? styles.topbarText : styles.topbarTextDisabled}>Regresar</Text>
+          </TouchableOpacity>
+        </View>
+        <WebView
+          ref={WEBVIEW_REF}
+          style={{flex: 1}}
+          onNavigationStateChange=
+            {this.onNavigationStateChange.bind(this)}
+          source={{uri: 'https://me-incluyo.org/places/'}}
+          /> 
+      </View>
       );
     }
+    onBack() {
+      this.refs[WEBVIEW_REF].goBack();
+    }
+  
+    onNavigationStateChange(navState) {
+      this.setState({
+        canGoBack: navState.canGoBack
+      });
+    }
+     
+
   }
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#F5FCFF',
+    },
+    topbar: {
+      height: 80,
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    topbarTextDisabled: {
+      color: 'gray'
+    }
+  });
+  
 
 
 export default Places;
